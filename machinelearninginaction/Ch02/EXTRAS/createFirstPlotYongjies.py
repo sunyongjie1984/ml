68e9afbc1288bc9d4f14e4ffe3f87dd7d14a1a95
yongjies <yongjies@cadence.com>
2018-07-25 20:11:22 +0800

    modify

--- machinelearninginaction/Ch02/EXTRAS/createFirstPlotYongjies.py
+++ machinelearninginaction/Ch02/EXTRAS/createFirstPlotYongjies.py
@@ -17,7 +17,7 @@ ax = fig.add_subplot( 1, 1, 1 );
 #datingDataMat,datingLabels = kNN.file2matrix('datingTestSet2.txt')
 datingDataMat, datingLabels = kNNYongjies.file2matrix( 'datingTestSet2.txt' );
 #ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
-ax.scatter( datingDataMat[ :, 1 ], datingDataMat[ :, 2 ], s = 15.0 * array( datingLabels ), c = 15.0 * array( datingLabels ) );
+ax.scatter( datingDataMat[ :, 1 ], datingDataMat[ :, 2 ], s = 45.0 * array( datingLabels ), c = 15.0 * array( datingLabels ) );
 ax.axis( [ -2, 25, -0.2, 2.0 ] );
 plt.grid( True );
 plt.xlabel( 'Percentage of Time Spent Playing Video Games' );
