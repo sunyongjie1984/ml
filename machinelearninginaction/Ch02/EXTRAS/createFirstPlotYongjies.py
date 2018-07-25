b8e791fa2269b6a0b19e5bdacf05be51c5ad27b6
yongjies <yongjies@cadence.com>
2018-07-25 20:06:05 +0800

    modify

--- machinelearninginaction/Ch02/EXTRAS/createFirstPlotYongjies.py
+++ machinelearninginaction/Ch02/EXTRAS/createFirstPlotYongjies.py
@@ -1,25 +1,25 @@
 '''
-Created on Oct 27, 2010
+Created on 20180725
 
-@author: Peter
+@author: yongjies
 '''
-from numpy import *
-#import kNN
+from numpy import *;
 import pdb;
-import sys
-sys.path.append("..")
-import kNN
-import matplotlib
-import matplotlib.pyplot as plt
-pdb.set_trace( );
-fig = plt.figure()
-ax = fig.add_subplot(111)
-#datingDataMat,datingLabels = kNN.file2matrix('datingTestSet.txt')
-datingDataMat,datingLabels = kNN.file2matrix('datingTestSet2.txt')
+import sys;
+sys.path.append("..");
+#import kNN
+import kNNYongjies;
+import matplotlib;
+import matplotlib.pyplot as plt;
+#pdb.set_trace( );
+fig = plt.figure( );
+ax = fig.add_subplot( 1, 1, 1 );
+#datingDataMat,datingLabels = kNN.file2matrix('datingTestSet2.txt')
+datingDataMat, datingLabels = kNNYongjies.file2matrix( 'datingTestSet2.txt' );
 #ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
-ax.scatter(datingDataMat[:,1], datingDataMat[:,2], s = 15.0*array(datingLabels), c=15.0*array(datingLabels))
-ax.axis([-2,25,-0.2,2.0])
+ax.scatter( datingDataMat[ :, 1 ], datingDataMat[ :, 2 ], s = 15.0 * array( datingLabels ), c = 15.0 * array( datingLabels ) );
+ax.axis( [ -2, 25, -0.2, 2.0 ] );
 plt.grid( True );
-plt.xlabel('Percentage of Time Spent Playing Video Games')
-plt.ylabel('Liters of Ice Cream Consumed Per Week')
-plt.show()
+plt.xlabel( 'Percentage of Time Spent Playing Video Games' );
+plt.ylabel( 'Liters of Ice Cream Consumed Per Week' );
+plt.show( );
