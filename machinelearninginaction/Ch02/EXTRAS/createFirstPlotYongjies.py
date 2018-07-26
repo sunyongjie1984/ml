7f0d9a622f67e6859b42262fb6a78de0d00b8b31
yongjies <yongjies@cadence.com>
2018-07-26 09:36:14 +0800

    mdofify

--- machinelearninginaction/Ch02/EXTRAS/createFirstPlotYongjies.py
+++ machinelearninginaction/Ch02/EXTRAS/createFirstPlotYongjies.py
@@ -13,7 +13,7 @@ import matplotlib;
 import matplotlib.pyplot as plt;
 #pdb.set_trace( );
 fig = plt.figure( );
-ax = fig.add_subplot( 1, 1, 1 );
+ax = fig.add_subplot( 2, 2, 4 );
 #datingDataMat,datingLabels = kNN.file2matrix('datingTestSet2.txt')
 datingDataMat, datingLabels = kNNYongjies.file2matrix( 'datingTestSet2.txt' );
 #ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
@@ -22,4 +22,17 @@ ax.axis( [ -2, 25, -0.2, 2.0 ] );
 plt.grid( True );
 plt.xlabel( 'Percentage of Time Spent Playing Video Games' );
 plt.ylabel( 'Liters of Ice Cream Consumed Per Week' );
+
+ax2 = fig.add_subplot( 1, 2, 1 );
+ax2.scatter( datingDataMat[ 0:5, 1 ], datingDataMat[ 0:5, 2 ], s = 45.0 * array( datingLabels[0:5] ), c = 15.0 * array( datingLabels[0:5] ) );
+ax2.axis( [ -2, 25, -0.2, 2.0 ] );
+for a, b  in zip( datingDataMat[ 0 : 5, 1 ], datingDataMat[ 0: 5, 2 ] ):
+    plt.annotate(
+            '(%s, %s)' %( a, b ),
+            xy = ( a, b ),
+            xytext = ( 0, -10 ),
+            textcoords = 'offset points',
+            ha = 'center',
+            va = 'top' );
+
 plt.show( );
